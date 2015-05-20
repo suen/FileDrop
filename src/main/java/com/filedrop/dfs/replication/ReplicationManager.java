dd4f5fec17ef9b43516ab99bc1bf57bfa4d416d1
Surendra <suren@surendradangol.com.np>
2015-05-20 16:59:44 +0200

    Replication feature

--- src/main/java/com/filedrop/dfs/replication/ReplicationManager.java
+++ src/main/java/com/filedrop/dfs/replication/ReplicationManager.java
@@ -1,6 +1,9 @@
 package com.filedrop.dfs.replication;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.filedrop.dfs.namenode.Node;
 import com.filedrop.dfs.namenode.NodeManager;
@@ -9,6 +12,8 @@ import com.filedrop.dfs.registry.model.Registry;
 
 public class ReplicationManager implements Runnable {
 
+	private static final int REPLICATION_FACTOR = 2;
+	
 	private NodeManager nodemanager;
 	private Registry registry;
 	private Thread thread;
@@ -27,23 +32,107 @@ public class ReplicationManager implements Runnable {
 		
 	}
 
-	private void queryRegistry(){
-		
-		nodemanager.getDatanodes();
-		
-		List<DFile> list = registry.list("/");
-
+	public void validateRegistryEntry(){
+		List<DFile> list = registry.getTree();		
 		for(DFile file: list ){
 			String[] datanode = registry.getDatanodeForFileID(file.getId());
 			
 			if (datanode == null){
 				System.out.println("ZOMBIE file detected " + 
-							file.getParent() + "/" + file.getName());
+						file.getPath());
+				
+				registry.rmfile(file.getPath());
+				continue;
 			}
 			
+			for(String dn: datanode){
+				Node node = nodemanager.getDatanodeByName(dn);
+				try {
+					if (! node.exists(file.getId())) {
+						System.out.println("False Entry detected " + 
+								file.getParent() + "/" + file.getName());
+						registry.rmmappingDN(file.getId(), dn);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			
+			datanode = registry.getDatanodeForFileID(file.getId());
+			if (datanode == null){
+				System.out.println("No more datanode for file : " + 
+						file.getPath());
+			}
+		}
+	}
+	
+	public List<Map<String, String>> getReport(){
+		validateRegistryEntry();
+		List<DFile> list = registry.getTree();
+		
+		List<Map<String, String>> result = new ArrayList<Map<String,String>>();
+		for(DFile file: list ){
+			String[] datanode = registry.getDatanodeForFileID(file.getId());
+
+			Map<String, String> map = new HashMap<String, String>();
+			
+			map.put("path", file.getPath());
+			map.put("id", file.getId());
+			map.put("factor", String.valueOf(datanode.length));
+
+			String dns = "";
+			for(String dn: datanode){
+				dns += "[" + dn + "] ";
+			}
+			map.put("datanodes", dns);
+			result.add(map);
 		}
 		
+		return result;
+
+	}
+	
+	public void replicateFile(int factor){
+		List<DFile> list = registry.getTree();		
+		for(DFile file: list ){
+			String[] dns = registry.getDatanodeForFileID(file.getId());
+			
+			if (dns == null){
+				System.out.println("replicateFile(): ZOMBIE file detected " + 
+						file.getPath());
+				continue;
+			}
+			
+			if (dns.length < factor){
+				Node repSourceNode = nodemanager.getDatanodeByName(dns[0]);
+				for (int i = dns.length; i<factor; i++) {
+					
+					//nodemanager.getDataNodesWithSpace(0);
+					
+				}
+				System.out.println("");
+			}
+			
+			for(String dn: dns){
+				Node node = nodemanager.getDatanodeByName(dn);
+				try {
+					if (! node.exists(file.getId())) {
+						System.out.println("False Entry detected " + 
+								file.getParent() + "/" + file.getName());
+						registry.rmmappingDN(file.getId(), dn);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			
+			dns = registry.getDatanodeForFileID(file.getId());
+			if (dns == null){
+				System.out.println("No more datanode for file : " + 
+						file.getPath());
+			}
 		
+		}		
 	}
 	
 	
